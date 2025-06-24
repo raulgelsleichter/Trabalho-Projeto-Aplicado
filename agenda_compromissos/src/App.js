4baa4b912aa439a98d201f6443601edfe17b6e47
Lipedev300 <luizfelipebasso22@gmail.com>
2025-06-24 11:04:28 -0300

    Rotas para navegação no site inicialmente configuradas

--- agenda_compromissos/src/App.js
+++ agenda_compromissos/src/App.js
@@ -1,17 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import Sobre from "./componentes/Sobre";
-import Footer from './componentes/Footer';
+import {BrowserRouter, Routes, Route, Link, useNavigate} from 'react-router-dom';
+import Footer from './componentes/Footer/Footer';
+import Sobre from './componentes/Sobre/Sobre';
+import Home from "./componentes/Home/Home";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home"> Página inicial</Link>
+            </li>
+            <li>
+              <Link to="/sobre">Sobre o projeto</Link>
+            </li>
+          </ul>
+      </nav>
+      <Routes>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/sobre' element = {<Sobre />}></Route>
+      </Routes>
+    </BrowserRouter>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Agenda pessoal</h1>
         <p> Organize seus compromissos de forma fácil e rápida</p>
       </header>
-      <Sobre></Sobre>
       <Footer></Footer>
     </div>
   );
---
+++ agenda_compromissos/src/componentes/Home/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h2> Página inicial</h2>
+            <p> Seja bem-vindo à página inicial da sua agenda pessoal. Aqui você terá acesso aos seus eventos já cadastrados</p>
+        </div>
+    )
+}
+
+export default Home;
