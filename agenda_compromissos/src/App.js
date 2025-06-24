dfa10b9cf8aac010c239c997cdd95ee2bf7f434d
Lipedev300 <luizfelipebasso22@gmail.com>
2025-06-24 11:54:31 -0300

    Criado componente Header, rotas ajeitadas e funcionando perfeitamente

--- agenda_compromissos/src/App.js
+++ agenda_compromissos/src/App.js
@@ -1,35 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route, Link, useNavigate} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, Link} from 'react-router-dom';
 import Footer from './componentes/Footer/Footer';
 import Sobre from './componentes/Sobre/Sobre';
 import Home from "./componentes/Home/Home";
+import Header from './componentes/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home"> Página inicial</Link>
-            </li>
-            <li>
-              <Link to="/sobre">Sobre o projeto</Link>
-            </li>
-          </ul>
-      </nav>
-      <Routes>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/sobre' element = {<Sobre />}></Route>
-      </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Header></Header>
+            <Routes>
+                <Route path='/home' element={<Home />}></Route>
+                <Route path='/sobre' element = {<Sobre />}></Route>
+            </Routes>
+        </BrowserRouter>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Agenda pessoal</h1>
-        <p> Organize seus compromissos de forma fácil e rápida</p>
-      </header>
       <Footer></Footer>
     </div>
   );
---
+++ agenda_compromissos/src/componentes/Header/Header.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div>
+            <h1> Agenda pessoal</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/home"> Página inicial</Link>
+                    </li>
+                    <li>
+                        <Link to="/sobre">Sobre o projeto</Link>
+                    </li>
+                </ul>
+            </nav>
+            <p> Organize seus compromissos de forma fácil e rápida</p>
+        </div>
+    )
+}
+
+export default Header;
